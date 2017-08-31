1633b2b2d5943018b57ab0809579e82f22ad3c7e
Roel Bindels <roelbindels@users.noreply.github.com>
2017-08-31 09:56:28 +0200

    Update utils.py

--- nginx_proxy/utils.py
+++ nginx_proxy/utils.py
@@ -1,10 +1,10 @@
-from stakkr import docker
+from stakkr import docker_actions
 from stakkr import stakkr_compose
 
 def connect_container_to_proxy(self):
     services = stakkr_compose.get_enabled_services(stakkr_compose.get_configured_services())
     if 'nginx-proxy-slave' in services:
         network = self.project_name + '_proxy-tier'
-        docker.add_container_to_network('nginx',network)
-        docker.add_container_to_network('nginx-gen', network)
-        docker.restart_container('{}_{}'.format(self.project_name,'apache'))
+        docker_actions.add_container_to_network('nginx',network)
+        docker_actions.add_container_to_network('nginx-gen', network)
+        docker_actions.restart_container('{}_{}'.format(self.project_name,'apache'))
